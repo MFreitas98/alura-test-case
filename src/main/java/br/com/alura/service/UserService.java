8ae8ef03309195d42c4f3ef5b3a29c63799f0d04
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-17 19:59:55 -0300

    feat: Creating Service class that contains: Create and find User methods

---
+++ src/main/java/br/com/alura/service/UserService.java
@@ -0,0 +1,37 @@
+package br.com.alura.service;
+
+import br.com.alura.exception.UserNotFoundException;
+import br.com.alura.mapper.UserMapper;
+import br.com.alura.model.dto.UserDto;
+import br.com.alura.model.dto.UserDtoRequest;
+import br.com.alura.model.entity.User;
+import br.com.alura.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    private final UserMapper mapper;
+
+    public UserDto findUserByUserName(String userName) {
+        log.info("UserService.findUserByUserName() -> init_process, userName {} ", userName);
+
+        User user = repository.findByUserName(userName).orElseThrow(UserNotFoundException::new);
+        return mapper.toDTO(user);
+    }
+
+    public void createUser(UserDtoRequest userDtoRequest) {
+        log.info("UserService.createUser() -> init_process, userDtoRequest {} ", userDtoRequest);
+
+        User entity = mapper.toEntity(userDtoRequest);
+
+        repository.save(entity);
+        log.info("UserService.createUser() -> finish_process");
+    }
+}
