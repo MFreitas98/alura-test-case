07a9212f401dcbacaeca044d53fe0072a553d544
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-17 19:46:03 -0300

    fix: Adding missing UserDtoRequest from previous commit

---
+++ src/main/java/br/com/alura/model/dto/UserDtoRequest.java
@@ -0,0 +1,17 @@
+package br.com.alura.model.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+
+
+@Getter
+@Setter
+@ToString
+@Schema(description = "Request to create an User on Alura API.")
+public class UserDtoRequest extends UserDto {
+
+    @Pattern(regexp = "^[a-z]{1,20}$", message = "The username must have a maximum of 20 characters, be lowercase only, and contain no numbers or spaces.")
+    private String userName;
+}
