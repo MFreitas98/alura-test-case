3f6b0d6e73cdacfa260ccd7a755c02a03da5f6bb
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-17 09:48:19 -0300

    feat: Creating migration of entity User

---
+++ src/main/resources/db/migrations/V1__create_table_user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user
+(
+    id         BIGINT AUTO_INCREMENT NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    user_name  VARCHAR(20)  NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    `role`     VARCHAR(255) NOT NULL,
+    created_at datetime     NOT NULL,
+    CONSTRAINT pk_user PRIMARY KEY (id)
+);
+
+ALTER TABLE user
+    ADD CONSTRAINT uc_user_email UNIQUE (email);
+
+ALTER TABLE user
+    ADD CONSTRAINT uc_user_user_name UNIQUE (user_name);
