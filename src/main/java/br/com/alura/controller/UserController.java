7631bed21bf19e70fec2a10881e267eb0d2dbc99
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-17 20:58:33 -0300

    feat: Adding user controller: post and get also swagger documentation

---
+++ src/main/java/br/com/alura/controller/UserController.java
@@ -0,0 +1,48 @@
+package br.com.alura.controller;
+
+import br.com.alura.controller.documentantion.UserControllerDocumentation;
+import br.com.alura.model.dto.UserDto;
+import br.com.alura.model.dto.UserDtoRequest;
+import br.com.alura.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+
+@RestController
+@RequestMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+@Slf4j
+@RequiredArgsConstructor
+public class UserController implements UserControllerDocumentation {
+
+    private final UserService userService;
+
+    @PostMapping(value = "/createUser", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<?> createUser(@Valid @RequestBody UserDtoRequest userDtoRequest) {
+
+        log.info("UserController.createUser() -> init_process");
+
+        userService.createUser(userDtoRequest);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{userName}/findUser")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<UserDto> findUserByUsername(@PathVariable String userName) {
+
+        log.info("UserController.findUserByUsername() -> init_process");
+
+        UserDto userDto = userService.findUserByUserName(userName);
+
+        log.info("UserController.findUserByUsername() -> finish_process, userDto {}", userDto);
+
+        return ResponseEntity.ok().body(userDto);
+    }
+}
---
+++ src/main/java/br/com/alura/controller/documentantion/UserControllerDocumentation.java
@@ -0,0 +1,39 @@
+package br.com.alura.controller.documentantion;
+
+
+import br.com.alura.model.dto.UserDtoRequest;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@Tag(name = "User Controller", description = "Controller to Create and find users.")
+public interface UserControllerDocumentation {
+
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    ResponseEntity<?> createUser(@Valid @RequestBody UserDtoRequest userDtoRequest);
+
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User found"),
+            @ApiResponse(responseCode = "204", description = "User not Found"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    ResponseEntity<?> findUserByUsername(@PathVariable String userName);
+
+}
