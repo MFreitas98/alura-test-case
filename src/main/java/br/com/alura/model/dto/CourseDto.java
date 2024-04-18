cf7f3bef6f5f364182b6622369af36c0efee0455
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 15:28:52 -0300

    feat: Creating CourseDto that will be able to fill creating and finding endpoints

---
+++ src/main/java/br/com/alura/model/dto/CourseDto.java
@@ -0,0 +1,45 @@
+package br.com.alura.model.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+
+@Getter
+@Setter
+@ToString
+@Schema(description = "CourseDtO from Alura API.")
+public class CourseDto implements Serializable {
+
+    @NotBlank(message = "Course name should not be null or empty.")
+    private String name;
+
+    @Pattern(regexp = "^[a-zA-Z]+(-[a-zA-Z]+)*$", message = "The course code must be alphabetic, without spaces, numeric characters, or special characters.")
+    private String code;
+
+    @NotNull
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Long instructorId;
+
+    private String description;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private boolean active;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private OffsetDateTime createdAt;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private OffsetDateTime inactivatedAt;
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
+    private UserDto instructor;
+}
