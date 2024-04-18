13909160cb1808040343c9b666dfa6789d1aa1b8
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 09:46:47 -0300

    fix: Fixing yml to not make Hibernate creating entitys on database and adding more configs to it

--- src/main/resources/db/migrations/V1__create_table_user.sql
+++ src/main/resources/db/migrations/V1__create_table_user.sql
@@ -1,16 +1,34 @@
+CREATE TABLE course
+(
+    id             BIGINT AUTO_INCREMENT NOT NULL,
+    code           VARCHAR(10)           NOT NULL,
+    instructor_id  BIGINT                NULL,
+    `description`  VARCHAR(255)          NOT NULL,
+    status         BIT(1)                NOT NULL,
+    created_at     datetime              NOT NULL,
+    inactivated_at datetime              NULL,
+    CONSTRAINT pk_course PRIMARY KEY (id)
+);
+
 CREATE TABLE user
 (
     id         BIGINT AUTO_INCREMENT NOT NULL,
-    name       VARCHAR(255) NOT NULL,
-    user_name  VARCHAR(20)  NOT NULL,
-    email      VARCHAR(255) NOT NULL,
-    `role`     VARCHAR(255) NOT NULL,
-    created_at datetime     NOT NULL,
+    name       VARCHAR(255)          NOT NULL,
+    user_name  VARCHAR(20)           NOT NULL,
+    email      VARCHAR(255)          NOT NULL,
+    `role`     VARCHAR(255)          NOT NULL,
+    created_at datetime              NOT NULL,
     CONSTRAINT pk_user PRIMARY KEY (id)
 );
+
+ALTER TABLE course
+    ADD CONSTRAINT uc_course_code UNIQUE (code);
 
 ALTER TABLE user
     ADD CONSTRAINT uc_user_email UNIQUE (email);
 
 ALTER TABLE user
     ADD CONSTRAINT uc_user_user_name UNIQUE (user_name);
+
+ALTER TABLE course
+    ADD CONSTRAINT FK_COURSE_ON_INSTRUCTOR FOREIGN KEY (instructor_id) REFERENCES user (id);
