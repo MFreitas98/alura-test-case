b4bbe87efa8c25ef1002a6f7f0cdfd360cf341c0
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 14:51:52 -0300

    feat: Creating User entity on model package

---
+++ src/main/java/br/com/alura/model/entity/Course.java
@@ -0,0 +1,44 @@
+package br.com.alura.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id")
+    private User instructor;
+
+    private String description;
+
+    private boolean status;
+
+    @Column(name = "created_at")
+    private OffsetDateTime createdAt;
+
+    @Column(name = "inactivated_at")
+    private OffsetDateTime inactivatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = OffsetDateTime.now();
+    }
+}
