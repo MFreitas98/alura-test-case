2e3a264d361ad73274094ac332456f6af944d988
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 20:16:16 -0300

    feat: Adding toEntity on EnrollmentMapper

--- src/main/java/br/com/alura/mapper/EnrollmentMapper.java
+++ src/main/java/br/com/alura/mapper/EnrollmentMapper.java
@@ -1,6 +1,6 @@
 package br.com.alura.mapper;
 
-import br.com.alura.model.dto.EnrollmentDTO;
+import br.com.alura.model.dto.EnrollmentDto;
 import br.com.alura.model.entity.Enrollment;
 import org.mapstruct.Mapper;
 import org.mapstruct.NullValueMappingStrategy;
@@ -9,5 +9,5 @@ import org.mapstruct.ReportingPolicy;
 @Mapper(componentModel = "spring", nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface EnrollmentMapper {
 
-    Enrollment toEntity(EnrollmentDTO enrollmentDTO);
+    Enrollment toEntity(EnrollmentDto enrollmentDTO);
 }
