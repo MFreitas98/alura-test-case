f28e4233fb921850b5e319ab1abb56ed0c1ab414
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 20:09:50 -0300

    feat: Creating create enrollment endpoint on controller and the swagger documentation for it.

--- src/main/java/br/com/alura/controller/EnrollmentController.java
+++ src/main/java/br/com/alura/controller/EnrollmentController.java
@@ -1,8 +1,16 @@
 package br.com.alura.controller;
 
+import br.com.alura.controller.documentantion.EnrollmentDocumentation;
+import br.com.alura.model.dto.EnrollmentDto;
+import br.com.alura.service.EnrollmentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/enrollments", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 @RequiredArgsConstructor
-public class EnrollmentController {
+public class EnrollmentController implements EnrollmentDocumentation {
+
+    private final EnrollmentService enrollmentService;
+
+    @PostMapping("/createEnrollment")
+    public ResponseEntity<?> createEnrollment(@Valid @RequestBody EnrollmentDto enrollmentDTO) {
+        log.info("EnrollmentController.createEnrollment() -> init_process");
+        enrollmentService.createEnrollment(enrollmentDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
--- src/main/java/br/com/alura/controller/documentantion/EnrollmentDocumentation.java
+++ src/main/java/br/com/alura/controller/documentantion/EnrollmentDocumentation.java
@@ -1,7 +1,24 @@
 package br.com.alura.controller.documentantion;
 
+import br.com.alura.model.dto.EnrollmentDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Enrollment Controller", description = "Controller to create Enrollments on Alura API.")
 public interface EnrollmentDocumentation {
+
+    @Operation(summary = "Create enrollment")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Enrollment Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    ResponseEntity<?> createEnrollment(@Valid @RequestBody EnrollmentDto enrollmentDTO);
 }
