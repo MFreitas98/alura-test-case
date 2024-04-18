9da8c52e026506d221a7c4209ac7edd2e2fceae8
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 17:31:14 -0300

    feat: Creating a GET Endpoint that list courser by a status delimiter and the documentation for it.

--- src/main/java/br/com/alura/controller/CourseController.java
+++ src/main/java/br/com/alura/controller/CourseController.java
@@ -3,10 +3,12 @@ package br.com.alura.controller;
 
 import br.com.alura.controller.documentantion.CourseControllerDocumentation;
 import br.com.alura.model.dto.CourseDto;
+import br.com.alura.model.enums.CourseStatusDelimiter;
 import br.com.alura.service.CourseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +27,18 @@ public class CourseController implements CourseControllerDocumentation {
         log.info("CourseController.createCourse() -> init_process");
         courseService.createCourse(courseDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("/listCoursesByStatusDelimiter")
+    public ResponseEntity<Page<CourseDto>> listCoursesByStatus(
+            @RequestParam(value = "status") CourseStatusDelimiter statusDelimiter,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        log.info("CourseController.createCourse() -> init_process, statusDelimiter {}, page {}, size {}",
+                statusDelimiter, page, size);
+
+        Page<CourseDto> coursePage = courseService.listCoursesByStatus(statusDelimiter, page, size);
+        log.info("CourseController.createCourse() -> finish_process");
+        return ResponseEntity.ok().body(coursePage);
     }
 }
--- src/main/java/br/com/alura/controller/documentantion/CourseControllerDocumentation.java
+++ src/main/java/br/com/alura/controller/documentantion/CourseControllerDocumentation.java
@@ -1,13 +1,16 @@
 package br.com.alura.controller.documentantion;
 
 import br.com.alura.model.dto.CourseDto;
+import br.com.alura.model.enums.CourseStatusDelimiter;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "Course Controller", description = "Controller to create, find and update Courses on Alura API.")
 public interface CourseControllerDocumentation {
@@ -22,4 +25,15 @@ public interface CourseControllerDocumentation {
             @ApiResponse(responseCode = "500", description = "Internal Server Error")})
     ResponseEntity<?> createCourse(@Valid @RequestBody CourseDto courseDto);
 
+    @Operation(summary = "List courses by StatusDelimiter")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved courses"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    ResponseEntity<Page<CourseDto>> listCoursesByStatus(
+            @RequestParam(value = "status", required = false) CourseStatusDelimiter statusDelimiter,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size);
 }
