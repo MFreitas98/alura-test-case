0079fa566d04783007b1783042202ba985d6c804
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 20:17:13 -0300

    fix: Fixing logic on create enrollment method to save it properly and throw exeception for not registered user on request

--- src/main/java/br/com/alura/service/EnrollmentService.java
+++ src/main/java/br/com/alura/service/EnrollmentService.java
@@ -2,9 +2,12 @@ package br.com.alura.service;
 
 import br.com.alura.exception.CourseNotRegisteredException;
 import br.com.alura.exception.UnprocessableEntityException;
+import br.com.alura.exception.UserNotRegisteredException;
 import br.com.alura.mapper.EnrollmentMapper;
-import br.com.alura.model.dto.EnrollmentDTO;
+import br.com.alura.model.dto.EnrollmentDto;
 import br.com.alura.model.entity.Course;
+import br.com.alura.model.entity.Enrollment;
+import br.com.alura.model.entity.User;
 import br.com.alura.repository.EnrollmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,12 +21,14 @@ public class EnrollmentService {
     private final EnrollmentRepository enrollmentRepository;
 
     private final CourseService courseService;
+    
+    private final UserService userService;
 
     private final EnrollmentMapper enrollmentMapper;
 
-    public void createEnrollment(EnrollmentDTO enrollmentDTO) {
+    public void createEnrollment(EnrollmentDto enrollmentDTO) {
         log.info("EnrollmentService.createEnrollment() -> init_process, enrollmentDTO {}", enrollmentDTO);
-        String courseCode = enrollmentDTO.getCourseDto().getCode();
+        String courseCode = enrollmentDTO.getCourseCode();
 
         Course course = courseService.findCourseOptionalByCode(courseCode)
                 .orElseThrow(CourseNotRegisteredException::new);
@@ -31,7 +36,15 @@ public class EnrollmentService {
         if (!course.isStatus()) {
             throw new UnprocessableEntityException("Course informed its not active.");
         }
-        enrollmentRepository.save(enrollmentMapper.toEntity(enrollmentDTO));
+
+        User user = userService.findUserOptionalByUserName(enrollmentDTO.getUserName())
+                .orElseThrow(UserNotRegisteredException::new);
+
+        Enrollment enrollment = enrollmentMapper.toEntity(enrollmentDTO);
+        enrollment.setCourse(course);
+        enrollment.setUser(user);
+
+        enrollmentRepository.save(enrollment);
 
         log.info("EnrollmentService.createEnrollment() -> finish_process");
     }
