b8333e88b78f6c0049c76a20a6d8886aa6eee940
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-18 22:36:38 -0300

    feat: Creating CourseEvalutationController and swagger documentation of it.

---
+++ src/main/java/br/com/alura/controller/documentation/CourseEvaluationControllerDocumentation.java
@@ -0,0 +1,7 @@
+package br.com.alura.controller.documentation;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "CourseEvaluation Controller", description = "Controller to create CourseEvaluation on Alura API.")
+public interface CourseEvaluationControllerDocumentation {
+}
