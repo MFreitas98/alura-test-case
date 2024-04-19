cf1a9727a84807c6333880e58ea8aa53f15caea8
MFreitas98 <marcelo.freitas@dasa.com.br>
2024-04-19 07:50:05 -0300

    feat: Creating POST course evalutation on controller and documentation for it.

--- src/main/java/br/com/alura/controller/CourseEvaluationController.java
+++ src/main/java/br/com/alura/controller/CourseEvaluationController.java
@@ -1,14 +1,30 @@
 package br.com.alura.controller;
 
+import br.com.alura.controller.documentation.CourseEvaluationControllerDocumentation;
+import br.com.alura.model.dto.CourseEvaluationDto;
+import br.com.alura.model.enums.ScoreValue;
+import br.com.alura.service.CourseEvaluationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/coursesEvaluations", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 @RequiredArgsConstructor
-public class CourseEvaluationController {
+public class CourseEvaluationController implements CourseEvaluationControllerDocumentation {
+
+    private final CourseEvaluationService courseEvaluationService;
+
+    @PostMapping("/createCourseEvaluation")
+    public ResponseEntity<?> createCourseEvaluation(@Valid @RequestBody CourseEvaluationDto courseEvaluationDto,
+                                                    @RequestParam("score") ScoreValue scoreValue) {
+        log.info("CourseEvaluationController.createCourseEvaluation() -> init_process");
+        courseEvaluationService.createCourseEvaluation(courseEvaluationDto, scoreValue);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 }
--- src/main/java/br/com/alura/controller/documentation/CourseEvaluationControllerDocumentation.java
+++ src/main/java/br/com/alura/controller/documentation/CourseEvaluationControllerDocumentation.java
@@ -1,7 +1,28 @@
 package br.com.alura.controller.documentation;
 
+import br.com.alura.model.dto.CourseEvaluationDto;
+import br.com.alura.model.dto.EnrollmentDto;
+import br.com.alura.model.enums.ScoreValue;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "CourseEvaluation Controller", description = "Controller to create CourseEvaluation on Alura API.")
 public interface CourseEvaluationControllerDocumentation {
+
+    @Operation(summary = "Create CourseEvaluation")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "CourseEvaluation Created"),
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized"),
+            @ApiResponse(responseCode = "403", description = "Forbidden"),
+            @ApiResponse(responseCode = "404", description = "Not Found"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")})
+    ResponseEntity<?> createCourseEvaluation(@Valid @RequestBody CourseEvaluationDto courseEvaluationDto,
+                                             @RequestParam("score") ScoreValue scoreValue);
 }
